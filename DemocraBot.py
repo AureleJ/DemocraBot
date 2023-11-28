f993dae89c6e03e8c7b6d8222cc8bbae9909a07c
NonorCoding <aurele.joblet@gmail.com>
2023-11-28 02:00:02 +0100

    Improves commandes, add winner command and remake votes command

--- DemocraBot.py
+++ DemocraBot.py
@@ -1,12 +1,15 @@
-import discord
 import settings
+import discord
 from discord.ext import commands
-from discord import app_commands
-from datetime import date, datetime
+from discord import ui, app_commands
+
+from discord.ui import Select, View
+
+from datetime import date, datetime, timedelta
 import time
 import json
-from typing import List
 from enum import Enum
+
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -29,81 +32,21 @@ async def on_ready():
         print(e)
 
 
-@bot.hybrid_command()
-async def voltaire(ctx, *, message):
-    await ctx.send(f"{message} : https://www.projet-voltaire.fr/")
-
-
-@bot.hybrid_command()
-async def train(ctx, *, member: discord.Member = None):
-    train = 'petit'
-
-    if member.id == 410111790970568705:
-        train = 'très grand'
-
-    await ctx.send(f"Effectivement d'après le théorème des trains {member.mention} a un {train} train !!! 🚂🚂")
-
-
-@bot.hybrid_command()
-async def aurele(ctx):
-    await ctx.send("c'est le plus bo")
-
-
-@bot.hybrid_command(name="ping", description="Make ping test")
-async def ping(ctx, message):
-    if message == None:
-        await ctx.send("Il manque un argument")
-    await ctx.send('pong')
-
-
-with open('lois.json', 'r', encoding='utf-8') as json_file:
-    laws_data = json.load(json_file)
-
-
-@bot.hybrid_command()
-async def lois(ctx, loi_id: int = None):
-    if loi_id is None:
-        await ctx.send("Veuillez preciser l'article souhaité (1 -> 7)")
-    else:
-        law = next(
-            (article for article in laws_data['articles'] if article['id'] == loi_id), None)
-        if law:
-            await ctx.send(f"**__{law['title']} :__**\n{law['content']}")
-        else:
-            await ctx.send("La loi n'existe pas.")
-
-
-@bot.hybrid_command()
-async def commisaire(ctx):
-    await ctx.send("🚨Attention la police rôde toujours🚨")
-
-
 class Sanctions(Enum):
     Ban = "Ban"
     Kick = "Kick"
     Mute = "Mute"
+    Aucune = "None"
 
 
 @bot.hybrid_command()
-async def jugement(ctx, membre: discord.Member = None, sanction: Sanctions = None, raison: str = None):
-    if membre is None:
-        await ctx.send("Veuillez mentionner un membre.")
-        return
-
-    if sanction is None:
-        await ctx.send("Veuillez spécifier une sanction.")
-        return
-
-    if raison is None:
-        await ctx.send("Veuillez spécifier une raison.")
-        return
-
+async def jugement(ctx, membre: discord.User, sanction: Sanctions, duree, raison):
     if sanction == Sanctions.Kick:
         await kick(ctx, membre, raison)
     elif sanction == Sanctions.Ban:
         await ban(ctx, membre, raison)
     elif sanction == Sanctions.Mute:
-        await mute(ctx, membre, raison)
+        await mute(ctx, membre, duree, raison)
 
     embed = discord.Embed(
         description=f"**Jugement de {membre.mention} par {ctx.author.top_role.mention}**",
@@ -119,6 +62,7 @@ async def jugement(ctx, membre: discord.Member = None, sanction: Sanctions = Non
     embed.set_thumbnail(url=membre.avatar.url)
 
     channel = bot.get_channel(1175946419081850902)
+
     if channel:
         await channel.send(embed=embed)
         await ctx.reply("Le jugement a bien été appliqué", ephemeral=True)
@@ -126,73 +70,75 @@ async def jugement(ctx, membre: discord.Member = None, sanction: Sanctions = Non
         await ctx.reply("Le salon de jugement n'a pas été trouvé. Veuillez contacter l'administrateur.")
 
 
-@bot.hybrid_command(description="Muter un membre.")
-@commands.has_permissions(manage_messages=True)
-async def mute(ctx, member: discord.Member, raison: str = None):
-    guild = ctx.guild
-    mutedRole = discord.utils.get(guild.roles, name="Muted")
+@bot.hybrid_command(name="mute", description="Muter un membre.")
+@commands.has_permissions(moderate_members=True)
+async def mute(ctx, membre: discord.Member, duree, raison):
 
-    if not mutedRole:
-        if ctx.guild.me.guild_permissions.manage_roles:
-            mutedRole = await guild.create_role(name="Muted")
+    if "s" in duree:
+        gettime = duree.strip("s")
+        newtime = timedelta(seconds=int(gettime))
 
-            for channel in guild.channels:
-                await channel.set_permissions(mutedRole, speak=False, send_messages=False, read_message_history=True, view_channel=True, read_messages=True)
+    if "m" in duree:
+        gettime = duree.strip("m")
+        newtime = timedelta(minutes=int(gettime))
 
-    if mutedRole not in member.roles:
-        await member.add_roles(mutedRole, reason=raison)
-        await ctx.reply(f"{member.mention} a été mute pour : {raison}", ephemeral=True)
-        await member.send(f"Vous avez été mute sur {guild.name} par {ctx.author.top_role} pour: {raison}")
-    else:
-        await ctx.reply(f"{member.mention} a déjà été mute.", ephemeral=True)
+    if "h" in duree:
+        gettime = duree.strip("h")
+        newtime = timedelta(hours=int(gettime))
 
+    if "j" in duree:
+        gettime = duree.strip("j")
+        newtime = timedelta(days=int(gettime))
 
-@bot.hybrid_command(description="Démuter un membre")
-@commands.has_permissions(manage_messages=True)
-async def unmute(ctx, member: discord.Member):
-    mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
-
-    if mutedRole in member.roles:
-        await member.remove_roles(mutedRole)
-        await ctx.reply(f"{member.mention} a été démuté")
-        await member.send(f"Vous avez été démute sur {ctx.guild.name}")
-    else:
-        await ctx.send(f"{member.mention} n'est pas mute")
+    await ctx.reply(f"{membre.mention} a été mute pour `{raison}` pendant `{str(duree)}`", ephemeral=True)
+    await membre.send(f"Vous avez été mute sur {ctx.guild.name} pendant {duree} par {ctx.author.top_role} pour: {raison}")
+    await membre.edit(timed_out_until=discord.utils.utcnow() + newtime, reason=raison)
 
 
-@bot.command(description="Bannir un membre")
+@bot.hybrid_command(name="unmute", description="Démuter un membre")
+@commands.has_permissions(moderate_members=True)
+async def unmute(ctx, membre: discord.Member):
+
+    await ctx.reply(f"{membre.mention} a été démuté`", ephemeral=True)
+    await membre.send(f"Vous avez été démute sur {ctx.guild.name}")
+    await membre.edit(timed_out_until=None)
+
+
+@bot.hybrid_command(description="Bannir un membre")
 @commands.has_permissions(ban_members=True)
-async def ban(ctx, member: discord.Member, raison: str = None):
-    await member.send(f"Vous avez été ban de {ctx.guild.name} par {ctx.author.top_role} pour: {raison}")
-    await member.ban(reason=raison)
-    await ctx.send(f"{member} a été banni")
+async def ban(ctx, membre: discord.Member, raison):
+    user = await bot.fetch_user(int(membre))
+
+    await ctx.reply(f"{membre.mention} a été ban pour `{raison}`", ephemeral=True)
+    await membre.send(f"Vous avez été ban sur {ctx.guild.name} par {ctx.author.top_role} pour: {raison}")
+    await ctx.guild.ban(user)
 
 
-@bot.command(description="Kick un membre")
-@commands.has_permissions(kick_members=True)
-async def kick(ctx, member: discord.Member, raison: str = None):
-    await member.send(f"Vous avez été ban de {ctx.guild.name} par {ctx.author.top_role} pour: {raison}")
-    await member.kick(reason=raison)
-    await ctx.send(f"{member} a été kick")
-
-
-@bot.command(description="débanir un membre")
+@bot.hybrid_command(description="Débannir un membre")
 @commands.has_permissions(ban_members=True)
-async def unban(ctx, *, member):
+async def unban(ctx, membre: discord.Member):
     bannedUsers = await ctx.guild.bans()
-    name, discriminator = member.split("#")
+    name, discriminator = membre.split("#")
 
     for ban in bannedUsers:
         user = ban.user
 
         if (user.name, user.discriminator) == (name, discriminator):
             await ctx.guild.unban(user)
-            await ctx.send(f"{user.mention} a été débani")
-            return
+            await ctx.reply(f"{user.mention} a été débani", ephemeral=True)
+            await membre.send(f"Vous avez été débani sur {ctx.guild.name}")
 
 
-@bot.hybrid_command()
-async def votes(ctx, duree: int, participants: str):
+@bot.hybrid_command(description="Kick un membre")
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, membre: discord.Member, raison):
+    await ctx.reply(f"{membre.mention} a été kick pour `{raison}`", ephemeral=True)
+    await membre.send(f"Vous avez été kick sur {ctx.guild.name} par {ctx.author.top_role} pour: {raison}")
+    await membre.kick(reason=raison)
+
+
+@bot.command()
+async def votes(ctx):
     await ctx.send(embed=discord.Embed(title="Jour des votes", description="Chers membres de la communauté, aujourd'hui est le grand jour des votes", color=0x007bff))
 
     participants = {
@@ -217,49 +163,49 @@ async def votes(ctx, duree: int, participants: str):
         embed2.set_thumbnail(url=user.avatar.url)
 
         await ctx.send(embed=embed2)
-        await ctx.send(view=MyView(user.name))
 
-vote_counts = {}
+    await votes_selections(ctx, participants)
+
 member_votes = {}
 
 
-class MyView(discord.ui.View):
-    def __init__(self, participant_name: str):
-        super().__init__()
-        self.participant_name = participant_name
+async def votes_selections(ctx, participants):
+    options = []
 
-    @discord.ui.button(label="Vote", style=discord.ButtonStyle.green)
-    async def button_callback(self, interaction: discord.Integration, button: discord.ui.Button):
-        vote_counts[self.participant_name] = vote_counts.get(
-            self.participant_name, 0) + 1
-        member_votes[interaction.user.name] = self.participant_name
-        print(
-            f"Vote pour {self.participant_name}. Nombre total de votes : {vote_counts}")
-        print(member_votes)
-        await interaction.response.send_message(f"Vous avez voté pour {self.participant_name}", ephemeral=True)
+    for participant_id in participants.values():
+        user = await bot.fetch_user(participant_id)
 
+        options.append(discord.SelectOption(label=user.global_name))
 
-@bot.command()
-async def result(ctx):
-    if member_votes != {}:
-        for participant, vote in vote_counts.items():
-            winner = participant
+    select = Select(placeholder="Votez ici", options=options)
 
-            if vote >= vote_counts[winner]:
-                winner = "Le vainqueur est : " + participant
-    else:
-        winner = "Le vote n'est pas encore terminé"
-    await ctx.send(winner)
+    async def callbacks(interaction):
+        await interaction.response.send_message(f"Vous avez voté pour {select.values[0]}", ephemeral=True)
+        member_votes[interaction.user.global_name] = select.values[0]
 
-
-@bot.hybrid_command()
-async def avatar(ctx, *,  member: discord.Member = None):
-    userAvatarUrl = member.avatar.url
-    await ctx.send(userAvatarUrl)
+    select.callback = callbacks
+    view = View()
+    view.add_item(select)
+    await ctx.send("Pour qui votez vous ?", view=view)
 
 
 @bot.command()
-async def dele(ctx):
+async def winner(ctx):
+    vote_counts = {}
+
+    for voted_for in member_votes.values():
+        if voted_for in vote_counts:
+            vote_counts[voted_for] += 1
+        else:
+            vote_counts[voted_for] = 1
+
+    winner = max(vote_counts, key=vote_counts.get)
+
+    await ctx.send(f"Le gagnant est {winner} avec {vote_counts[winner]} votes!")
+
+
+@bot.command()
+async def del_casier(ctx):
     messages = [message async for message in ctx.channel.history(limit=123, oldest_first=True)]
     # print(messages[0].content)
     posted = messages[0].created_at.date().strftime('%d')
@@ -275,4 +221,47 @@ async def dele(ctx):
         time.sleep(2)
         await botMessage.delete()
         await ctx.message.delete()
+
+
+@bot.hybrid_command()
+async def voltaire(ctx, *, message):
+    await ctx.send(f"{message} : https://www.projet-voltaire.fr/")
+
+
+@bot.hybrid_command()
+async def train(ctx, *, member: discord.Member = None):
+    train = 'petit'
+
+    if member.id == 410111790970568705:
+        train = 'très grand'
+
+    await ctx.send(f"Effectivement d'après le théorème des trains {member.mention} a un {train} train !!! 🚂🚂")
+
+
+@bot.hybrid_command()
+async def aurele(ctx):
+    await ctx.send("c'est le plus bo")
+
+
+with open('lois.json', 'r', encoding='utf-8') as json_file:
+    laws_data = json.load(json_file)
+
+
+@bot.hybrid_command()
+async def lois(ctx, loi_id: int = None):
+    if loi_id is None:
+        await ctx.send("Veuillez preciser l'article souhaité (1 -> 7)")
+    else:
+        law = next(
+            (article for article in laws_data['articles'] if article['id'] == loi_id), None)
+        if law:
+            await ctx.send(f"**__{law['title']} :__**\n{law['content']}")
+        else:
+            await ctx.send("La loi n'existe pas.")
+
+
+@bot.hybrid_command()
+async def commisaire(ctx):
+    await ctx.send("🚨Attention la police rôde toujours🚨")
+
 bot.run(settings.api_key)
